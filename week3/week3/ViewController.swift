a5aa4f25dcc084dab077c55a490db63db15ed6b6
Arch Patel <73293199+archp4@users.noreply.github.com>
2025-01-31 12:50:49 -0500

    Week 3 Start

---
+++ week3/week3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week3
+//
+//  Created by Arch Umeshbhai Patel on 2025-01-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
