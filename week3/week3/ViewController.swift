c2b6269ed2bb83df0b01feca1afb726c63674408
Arch Patel <73293199+archp4@users.noreply.github.com>
2025-01-31 13:42:40 -0500

    TableViewDemo
    Learn about TableView and Cells

--- week3/week3/ViewController.swift
+++ week3/week3/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        studentTable.dataSource = self
+        studentTable.delegate = self
     }
-
+    @IBOutlet weak var selectedRow: UILabel!
+    
+    var collegeList = ["Humber College", "Centennial College", "Seneca College"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return collegeList.count
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedRow.text = "Selected College: \(collegeList[indexPath.row])"
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // var cell  = UITableViewCell()
+        let cell2 = tableView.dequeueReusableCell(withIdentifier: "mycell")!
+        cell2.textLabel?.text = collegeList[indexPath.row]
+        return cell2
+    }
+    
+    @IBOutlet weak var studentTable: UITableView!
+    
+    
 
 }
 
